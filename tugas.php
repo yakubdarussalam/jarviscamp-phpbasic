2c9716b6c78d9b5508b1d871073d51238e1a075c
mrkubs <74693732+mrkubs@users.noreply.github.com>
2023-07-09 09:31:05 +0700

    Update tugas.php

--- tugas.php
+++ tugas.php
@@ -1,5 +1,5 @@
 <?php
-//Funftion untuk pembagian dua parameter
+//Fungtion untuk pembagian dua parameter
 function pembagian ($nilai1,$nilai2){
     $hasil = $nilai1 / $nilai2;
     echo "Hasil Pembagian dari $nilai1 : $nilai2 adalah ";
